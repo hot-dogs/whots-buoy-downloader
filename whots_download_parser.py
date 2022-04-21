9f4438a19cd00d7cf260020d0f59bd34772e51b2
fcarvalhopacheco <fcarvalho.pacheco@gmail.com>
2022-04-20 19:40:46 -1000

    added len(sys.argv) <=4 to make sure user typed all commands"

--- whots_download_parser.py
+++ whots_download_parser.py
@@ -1,7 +1,41 @@
 #!/usr/bin/env python3
 import argparse
 import roman
+import sys
 from urllib.request import urlopen
+
+
+def parse_args():
+    """
+      Parse input arguments
+    """
+    parser = argparse.ArgumentParser(description=' Download WHOTS files')
+
+    parser.add_argument(
+        "-w",
+        type=int,
+        nargs=1,
+        dest="whots_number",
+        help="Type the WHOTS buoy number (eg. `17` for WHOTS-17)",
+        action="store"
+    )
+
+    parser.add_argument(
+        "-s",
+        type=int,
+        nargs=1,
+        dest="system_number",
+        help="WHOTS system number: ( 1 or 2 )",
+        action="store"
+    )
+
+    if len(sys.argv) <= 4:
+        parser.print_help()
+        sys.exit()
+    else:
+        args = parser.parse_args()
+
+    return args
 
 
 class WhotsFileDownloader:
@@ -11,44 +45,20 @@ class WhotsFileDownloader:
     """
 
     def __init__(self, whots_number, system_number):
-        self.whots_number, self.system_number, self.content, self.read_file = whots_number, system_number, None, None
-
-    def parse_args():
-        """
-          Parse input arguments
-        """
-        parser = argparse.ArgumentParser(description=' Download WHOTS files')
-
-        parser.add_argument(
-            "-w",
-            type=int,
-            nargs=1,
-            dest="whots_number",
-            help="Type the WHOTS buoy number (eg. `17` for WHOTS-17)",
-            action="store"
-        )
-
-        parser.add_argument(
-            "-s",
-            type=int,
-            nargs=1,
-            dest="system_number",
-            help="WHOTS system number: ( 1 or 2 )",
-            action="store"
-        )
-
-        args = parser.parse_args()
-        return args
+        self.whots_number = whots_number
+        self.system_number = system_number
+        self.content = None
+        self.read_file = None
 
     def get_whots_system(self):
         if self.system_number == 1:
             self.content = "https://uop.whoi.edu/currentprojects/WHOTS/data/WHOTS-" + \
-                           str(roman.toRoman(self.whots_number)) +\
+                           str(roman.toRoman(self.whots_number)) + \
                            "_MET_sys1.txt "
 
         elif self.system_number == 2:
             self.content = "https://uop.whoi.edu/currentprojects/WHOTS/data/WHOTS-" + \
-                           str(roman.toRoman(self.whots_number)) +\
+                           str(roman.toRoman(self.whots_number)) + \
                            "_MET_sys2.txt "
 
         print(self.content)
@@ -60,24 +70,24 @@ class WhotsFileDownloader:
         return self.read_file
 
     def save_whots_sys(self):
-        with open("WHOTS-"+
+        with open("WHOTS-" +
                   str(roman.toRoman(self.whots_number)) +
-                  "_MET_sys"+ str(self.system_number) +
+                  "_MET_sys" + str(self.system_number) +
                   ".txt", 'w') as output:
             output.write(self.read_file)
 
         print("Saving ... " + "WHOTS-" +
               str(roman.toRoman(self.whots_number)) +
-              "_MET_sys"+str(self.system_number)+".txt")
+              "_MET_sys" + str(self.system_number) + ".txt")
         return
 
 
 if __name__ == "__main__":
-    args = WhotsFileDownloader.parse_args()
-    download = WhotsFileDownloader(args.whots_number[0], args.system_number[0])
-    download.get_whots_system()
-    download.read_whots_sys()
-    download.save_whots_sys()
+    args = parse_args()
+    whots = WhotsFileDownloader(args.whots_number[0], args.system_number[0])
+    whots.get_whots_system()
+    whots.read_whots_sys()
+    whots.save_whots_sys()
 
     # THIS ALSO WORKS
     # args = WhotsFileDownloader(17, 1)
