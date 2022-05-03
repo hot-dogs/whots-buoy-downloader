4e2af89cb8bedaebd4c91fe7f3aafcd95f7989f1
fcarvalhopacheco <fcarvalho.pacheco@gmail.com>
2022-05-02 19:54:22 -1000

    added tests

--- src/tests/test_make_whots_dataset.py
+++ src/tests/test_make_whots_dataset.py
@@ -1,3 +1,28 @@
-from whots_data_downloader
-def test_init():
-    assert True
+import os
+from subprocess import getstatusoutput, getoutput
+
+prg = '../data/make_whots_dataset.py'
+
+
+def test_exist():
+    """exist"""
+    assert os.path.isfile(prg)
+
+
+def test_usage():
+    """usage"""
+
+    for flag in ['-w', '-whots_number']:
+        rv, out = getstatusoutput(f'{prg} {flag}')
+        assert rv == 0
+        assert out.lower().startswith('usage')
+
+
+def test_input():
+    """test for input"""
+
+    option_1 = '-w'
+    option_2 = '-s'
+
+    rv, out = getstatusoutput(f"{prg} {option_1} {'17'} {option_2} {'1'}")
+    assert out.endswith('WHOTS-XVII_MET_sys1.txt')
