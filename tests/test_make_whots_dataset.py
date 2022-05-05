23b4cb3a21d6f860307baf49728fa450093f01fa
fcarvalhopacheco <fcarvalho.pacheco@gmail.com>
2022-05-04 22:31:00 -1000

    fixed/created new tests

--- tests/test_make_whots_dataset.py
+++ tests/test_make_whots_dataset.py
@@ -1,44 +1,23 @@
 from subprocess import getstatusoutput, call
-
-# def test_usage():
-#     """usage"""
-#
-#     for flag in ['-w', '-whots_number']:
-#         status, output = getstatusoutput(f'{PRG} {flag}')
-#         assert status == 0
-#         assert output.lower().startswith('usage')
-#
-#
-# def test_input():
-#     """test for input"""
-#     option_1 = '-w'
-#     option_2 = '-s'
-#
-#     status, output = getstatusoutput(f"{PRG} {option_1} {'17'} {option_2} {'1'}")
-#     assert output.endswith('WHOTS-XVII_MET_sys1.txt')
-#     call('rm -rf ../../data/raw/WHOTS-XVII_MET_sys1.txt', shell=True)
-#
-#
-# def test_whots_system_number():
-#     whots_17_1 = WhotsFileDownloader(17, 1)
-#     assert repr(whots_17_1) == "WhotsFileDownloader(17,1)"
-#
-#
-# def test_get_url():
-#     whots_16_2 = WhotsFileDownloader(16, 2)
-from data.make_whots_dataset import WhotsFileDownloader
-
-WHOTS_17_1 = WhotsFileDownloader(17, 1)
+import pytest
+import data.make_whots_dataset as mwd
 
 
 def test_construction():
     """Test the class construction"""
-    assert WHOTS_17_1
+    whots_17_1 = mwd.DefineWhotsSystem(17, 1)
+    assert whots_17_1.whots_number == 17
+    assert whots_17_1.system_number == 1
+
+    whots_50_1 = mwd.DefineWhotsSystem(50, 1)
+    assert whots_50_1.whots_number == 50
+    assert whots_50_1.system_number == 1
 
 
 def test_get_url():
     """Test get_url function"""
-    assert WHOTS_17_1.get_url() == 'https://uop.whoi.edu/currentprojects/WHOTS/data/WHOTS-XVII_MET_sys1.txt'
+    whots_17_1 = mwd.DefineWhotsSystem(17, 1)
+    assert whots_17_1.url() == 'https://uop.whoi.edu/currentprojects/WHOTS/data/WHOTS-XVII_MET_sys1.txt'
 
 
 # def test_test_url():
@@ -55,3 +34,23 @@ def test_get_url():
 #
 # def test_display_system_file():
 #     assert False
+
+@pytest.mark.skip(reason='Need to fix/implement the bash usage')
+def test_usage():
+    """usage"""
+
+    for flag in ['-w', '-whots_number']:
+        status, output = getstatusoutput(f'{PRG} {flag}')
+        assert status == 0
+        assert output.lower().startswith('usage')
+
+
+@pytest.mark.skip(reason='Need to fix/implement bash input')
+def test_input():
+    """test for input"""
+    option_1 = '-w'
+    option_2 = '-s'
+
+    status, output = getstatusoutput(f"{PRG} {option_1} {'17'} {option_2} {'1'}")
+    assert output.endswith('WHOTS-XVII_MET_sys1.txt')
+    call('rm -rf ../../data/raw/WHOTS-XVII_MET_sys1.txt', shell=True)
