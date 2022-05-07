6303b656679246cd3bd8c7d723e49a0a972a8034
fcarvalhopacheco <fcarvalho.pacheco@gmail.com>
2022-05-07 00:12:27 -1000

    playing with @property

--- src/data/make_whots_dataset.py
+++ src/data/make_whots_dataset.py
@@ -3,7 +3,6 @@ import roman
 import sys
 from urllib.request import urlopen
 from urllib.error import URLError, HTTPError
-
 from data.whots_parse import get_args
 
 
@@ -15,27 +14,43 @@ class DefineWhotsSystem:
     """
 
     def __init__(self, whots_number, system_number):
-        self.whots_number = whots_number
-        self.system_number = system_number
+        self._whots_number = whots_number
+        self._system_number = system_number
         self.content = None
 
     def __str__(self):
         self_str = (
-            f' WHOTS NUMBER :  {self.whots_number}\n'
-            f' SYSTEM NUMBER:  {self.system_number}'
+            f'  WHOTS NUMBER :  {self._whots_number}'
+            f'\nSYSTEM NUMBER:  {self._system_number}'
         )
         return self_str
 
     def __repr__(self):
-        return f"{self.__class__.__name__}({self.whots_number},{self.system_number})"
+        return f"{self.__class__.__name__}({self._whots_number},{self._system_number})"
+
+    @property
+    def whots_number(self):
+        return self._whots_number
+
+    @whots_number.setter
+    def whots_number(self, value):
+        self._whots_number = value
+
+    @property
+    def system_number(self):
+        return self._system_number
+
+    @system_number.setter
+    def system_number(self, value):
+        self._system_number = value
 
     def url(self):
         """.url is a property that formats and returns the correct
         WHOTS file.
         """
         self.content = "https://uop.whoi.edu/currentprojects/WHOTS/data/WHOTS-" + \
-                       str(roman.toRoman(self.whots_number)) + \
-                       "_MET_sys" + str(self.system_number) + ".txt"
+                       str(roman.toRoman(self._whots_number)) + \
+                       "_MET_sys" + str(self._system_number) + ".txt"
 
         return self.content
 
@@ -44,13 +59,13 @@ class DefineWhotsSystem:
         try:
             urlopen(self.content)
         except HTTPError as e:
-            sys.exit(f'WHOTS-{self.whots_number} SYSTEM-{self.system_number} is not available.\n'
+            sys.exit(f'WHOTS-{self._whots_number} SYSTEM-{self._system_number} is not available.\n'
                      f"{'-' * 70}\n"
                      f"The Error code was: {e.code}\n"
                      f"{'-' * 70}\n")
 
         except URLError as e:
-            sys.exit(f'WHOTS-{self.whots_number} SYSTEM-{self.system_number} is not available.\n'
+            sys.exit(f'WHOTS-{self._whots_number} SYSTEM-{self._system_number} is not available.\n'
                      f"{'-' * 70}\n"
                      f"Failed to reach the serve:"
                      f"Reason: {e.reason}"
@@ -62,20 +77,20 @@ class DefineWhotsSystem:
 
     def save_raw_data(self):
         with open(os.path.join('../../data/raw', "WHOTS-" +
-                                                 str(roman.toRoman(self.whots_number)) +
-                                                 "_MET_sys" + str(self.system_number) +
+                                                 str(roman.toRoman(self._whots_number)) +
+                                                 "_MET_sys" + str(self._system_number) +
                                                  ".txt"), 'w') as output:
             return output.write(self.read_raw_file())
 
     def display_system_file(self):
         return print("-" * 70 + "\nSaving ... " + "WHOTS-" +
-                     str(roman.toRoman(self.whots_number)) +
-                     "_MET_sys" + str(self.system_number) + ".txt")
+                     str(roman.toRoman(self._whots_number)) +
+                     "_MET_sys" + str(self._system_number) + ".txt")
 
 
 def main():
     args = get_args()
-    whots = DefineWhotsSystem(args.whots_number[0], args.system_number[0])
+    whots = DefineWhotsSystem(args.__whots_number[0], args.__system_number[0])
     whots.url()
     whots.test_url()
     whots.read_raw_file()
